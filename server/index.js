e793c3323c683c675d2afdc788311bb10479cf33
aminiyolo <73641636+aminiyolo@users.noreply.github.com>
2021-11-04 19:13:13 +0900

    path added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config();
 
 const userRoute = require("./routes/user");
